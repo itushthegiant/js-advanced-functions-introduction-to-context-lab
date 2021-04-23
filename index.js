1c5b90de11128ac728140b542de2dd5f39f914e8
etamar <etamar.levy@gmail.com>
2021-04-23 16:09:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+let array = ["Gray", "Worm", "Security", 1];
+
+function createEmployeeRecord(array) {
+    let testEmployee = {
+        firstName: `${array[0]}`,
+        familyName: `${array[1]}`,
+        title: `${array[2]}`,
+        payPerHour: 1,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return testEmployee;
+}
+
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+  ]
+
+function createEmployeeRecords() {
+    
+}
+
+
